f53fa9dcb1c42b32f61d7d28ceb467c7a76ea723
vm4321minus <>
2021-07-03 19:57:53 +0300

    feat: min, max, avg array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+  return array.reduce((z, x) => (z + x)) / array.length;
 }
